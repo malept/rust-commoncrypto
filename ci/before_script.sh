3f73144922fc3d8605be9ef6388e0aea8ed9800b
Mark Lee <commoncrypto-sys@lazymalevolence.com>
2016-11-19 17:31:38 -0800

    Travis: install pip if it doesn't exist

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [[ -z $(which pip) ]]; then
+    curl -sSLO https://bootstrap.pypa.io/get-pip.py
+	python get-pip.py --user
+fi
+
 # load travis-cargo
 pip install 'travis-cargo<0.2' --user
 
