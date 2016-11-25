7763131414d1e31a1d64ae502faf2eb53e97e9d3
Mark Lee <commoncrypto-sys@lazymalevolence.com>
2016-11-24 17:43:36 -0800

    Move commoncrypto-sys to a subcrate

--- ci/script.sh
+++ ci/script.sh
@@ -3,10 +3,14 @@
 # the main build
 set -e
 
-travis-cargo build
-travis-cargo test
-travis-cargo --only stable doc
+run_cargo() {
+    pushd "$1"
+    travis-cargo test
+    travis-cargo --only stable doc
+    if test "$TRAVIS_RUST_VERSION" = "nightly"; then
+        cargo clippy --features=$TRAVIS_CARGO_NIGHTLY_FEATURE -- -Wclippy_pedantic
+    fi
+    popd
+}
 
-if test "$TRAVIS_RUST_VERSION" = "nightly"; then
-    cargo clippy --features=$TRAVIS_CARGO_NIGHTLY_FEATURE -- -Wclippy_pedantic
-fi
+run_cargo commoncrypto-sys
