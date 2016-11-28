ce757954c456cabfeeafa0f8578ebb43a8e9258f
Mark Lee <commoncrypto-sys@lazymalevolence.com>
2016-11-27 19:12:21 -0800

    Travis: only run rustfmt if Rust files were changed

--- ci/script.sh
+++ ci/script.sh
@@ -5,17 +5,26 @@ set -e
 
 run_cargo() {
     pushd "$1"
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-        cargo fmt -- --write-mode=diff $(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" | grep \.rs$)
-    else
-        cargo fmt -- --write-mode=diff $(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" | sort -u | grep \.rs$)
-    fi
+
+    run_rustfmt
     travis-cargo test
     travis-cargo --only stable doc
     if test "$TRAVIS_RUST_VERSION" = "nightly"; then
         cargo clippy --features=$TRAVIS_CARGO_NIGHTLY_FEATURE -- -Wclippy_pedantic
     fi
     popd
+}
+
+run_rustfmt() {
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+        files="$(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" | grep \.rs$)"
+    else
+        files="$(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" | sort -u | grep \.rs$)"
+    fi
+
+    if [[ -n "$files" ]]; then
+        cargo fmt -- --write-mode=diff $files
+    fi
 }
 
 run_cargo commoncrypto-sys
