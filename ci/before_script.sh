8f008dd4b5607cabb718ef3ef8562ab4418c1d88
Mark Lee <commoncrypto-sys@lazymalevolence.com>
2017-07-04 11:37:58 -0700

    Replace travis-cargo with cargo-travis

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
+cargo_install() {
+    local binary="$1"
+    local crate="$2"
 
-if [[ -z $(which pip) ]]; then
-    curl -sSLO https://bootstrap.pypa.io/get-pip.py
-	python get-pip.py --user
-fi
+    test -x $HOME/.cargo/bin/$binary || cargo install $crate
+}
 
-# load travis-cargo
-pip install 'travis-cargo<0.2' --user
+cargo_install_update() {
+    local binary="$1"
+    local crate="$2"
 
-test -x $HOME/.cargo/bin/cargo-install-update || cargo install cargo-update
-test -x $HOME/.cargo/bin/rustfmt || cargo install rustfmt
-cargo install-update rustfmt
+    cargo_install "$binary" "$crate"
+    cargo install-update $crate
+}
 
-if test "$TRAVIS_RUST_VERSION" = "nightly"; then
-    test -x $HOME/.cargo/bin/cargo-clippy || cargo install clippy
-    cargo install-update clippy
+cargo_install cargo-install-update cargo-update
+cargo_install_update rustfmt rustfmt
+
+if test "$TRAVIS_RUST_VERSION" = "stable"; then
+    cargo_install_update cargo-coverage cargo-travis
+elif test "$TRAVIS_RUST_VERSION" = "nightly"; then
+    cargo_install_update cargo-clippy clippy
 fi
--- ci/script.sh
+++ ci/script.sh
@@ -1,37 +1,40 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-# the main build
-set -e
-
-run_cargo() {
+run_clippy() {
     dir="$1"
     pushd "$dir" > /dev/null
 
-    # run_rustfmt
-    travis-cargo test
-    travis-cargo --only stable doc
+    # https://github.com/Manishearth/rust-clippy/issues/1707
     if test "$TRAVIS_RUST_VERSION" = "nightly"; then
-        cargo clippy -- --allow clippy_pedantic
+        cargo +nightly clippy -- --allow clippy_pedantic
+        echo "Completed clippy in $dir"
     fi
     popd > /dev/null
-    echo "Completed $dir"
 }
 
 #
 # Based on comments in https://www.reddit.com/r/rust/comments/5f3oov/
 #
 run_rustfmt() {
+    local changed_rust_files
     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-        files="$(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" . | (grep \.rs$; true))"
+        changed_rust_files="$(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" . | (grep \.rs$; true))"
     else
-        files="$(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" . | sort -u | (grep \.rs$; true))"
+        changed_rust_files="$(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" . | sort -u | (grep \.rs$; true))"
     fi
 
-    if [[ -n "$files" ]]; then
-        cargo fmt -- --write-mode=diff $files
+    if [[ -n "$changed_rust_files" ]]; then
+        cargo fmt --all -- --write-mode=diff $changed_rust_files
     fi
     echo "Completed rustfmt"
 }
 
-run_cargo commoncrypto-sys
-run_cargo commoncrypto
+cargo test --all
+if test "$TRAVIS_RUST_VERSION" = "stable"; then
+    cargo doc --all
+fi
+
+run_rustfmt
+
+run_clippy commoncrypto-sys
+run_clippy commoncrypto
